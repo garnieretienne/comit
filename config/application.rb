f87cc90336c7f21114a057b4d945e60c27bb8f98
Etienne Garnier <garnier.etienne@gmail.com>
2013-04-08 00:16:32 +0200

    support for heroku assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module Comit
   class Application < Rails::Application
+    # Support for heroku assets pipeline with rails 3
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
